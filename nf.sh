00e7a7955f91d27fcec4e3bfdb442c359b9523c6
nf-testsh <cjzq6806@outlook.com>
2024-07-27 11:37:25 +0800

    Update nf.sh

--- nf.sh
+++ nf.sh
@@ -41,18 +41,18 @@ download_extra_data() {
  result2=$(curl ${CURL_DEFAULT_OPTS} -fsL 'https://www.netflix.com/title/70143836' -w %{http_code} -o /dev/null -H 'host: www.netflix.com' -H 'accept-language: en-US,en;q=0.9' -H "sec-ch-ua: ${UA_SEC_CH_UA}" -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Windows"' -H 'sec-fetch-site: none' -H 'sec-fetch-mode: navigate' -H 'sec-fetch-user: ?1' -H 'sec-fetch-dest: document' --user-agent "${UA_BROWSER}")
 
     if [ "${result1}" == '000' ] || [ "$result2" == '000' ]; then
-        echo -n -e "\r Netflix:\t\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
+        echo -n -e "\r Netflix:\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
     fi
     if [ "$result1" == '404' ] && [ "$result2" == '404' ]; then
-        echo -n -e "\r Netflix:\t\t\t\t${Font_Yellow}Originals Only${Font_Suffix}\n"
+        echo -n -e "\r Netflix:\t\t${Font_Yellow}Originals Only${Font_Suffix}\n"
     fi
     if [ "$result1" == '403' ] || [ "$result2" == '403' ]; then
-        echo -n -e "\r Netflix:\t\t\t\t${Font_Red}No${Font_Suffix}\n"
+        echo -n -e "\r Netflix:\t\t${Font_Red}No${Font_Suffix}\n"
     fi
     if [ "$result1" == '200' ] || [ "$result2" == '200' ]; then
          tmpresult=$(curl ${CURL_DEFAULT_OPTS} -sL 'https://www.netflix.com/' -H 'accept-language: en-US,en;q=0.9' -H "sec-ch-ua: ${UA_SEC_CH_UA}" -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Windows"' -H 'sec-fetch-site: none' -H 'sec-fetch-mode: navigate' -H 'sec-fetch-user: ?1' -H 'sec-fetch-dest: document' --user-agent "${UA_BROWSER}")
          region=$(echo "$tmpresult" | grep -woP '"requestCountry":{"id":"\K\w\w' | head -n 1)
-        echo -n -e "\r Netflix:\t\t\t\t${Font_Green}Yes (Region: ${region})${Font_Suffix}\n"
+        echo -n -e "\r Netflix:\t\t${Font_Green}Yes (Region: ${region})${Font_Suffix}\n"
     fi
 
-    echo -n -e "\r Netflix:\t\t\t\t\t${Font_Red}Failed (Error: ${result1}_${result2})${Font_Suffix}\n"
+    echo -n -e "\r Netflix:\t\t\t${Font_Red}Failed (Error: ${result1}_${result2})${Font_Suffix}\n"
