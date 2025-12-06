62b9eab0e90e25f075d0e29966c3b523bacd71ab
nf-testsh <cjzq6806@outlook.com>
2025-12-06 23:07:56 +0800

    Update nf.sh

--- nf.sh
+++ nf.sh
@@ -61,10 +61,10 @@ elif [[ "$STATUS_CODE" == "200" ]]; then
         echo -e "${Font_Green}您目前完整解锁非自制剧 || (解锁地区: ${REGION})${Font_Suffix}"
     else
         # 200 OK 但没有特征码
-        echo -e "${Font_Red}检测异常 (ip无法解锁)${Font_Suffix}"
+        echo -e "${Font_Red}检测异常 (ip无法解锁或仅支持自制)${Font_Suffix}"
     fi
 
 else
     # 其他状态码
-    echo -e "${Font_Red}检测异常 (状态码: $STATUS_CODE)${Font_Suffix}"
+    echo -e "${Font_Red}检测失败 (状态码: $STATUS_CODE)${Font_Suffix}"
 fi
