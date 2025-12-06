71bd6e71c0555e2c0a5af8757b8c106ec5472763
nf-testsh <cjzq6806@outlook.com>
2025-12-06 23:17:46 +0800

    Update nf.sh

--- nf.sh
+++ nf.sh
@@ -49,7 +49,7 @@ elif [[ "$STATUS_CODE" == "200" ]]; then
         
         # --- 确认解锁成功，现在才检测地区 (优化速度) ---
         # 尝试访问 login 页面看跳转地址
-        REGION_URL=$(curl -fsSI -X GET -A "$UA" --max-time 5 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/login" 2>/dev/null)
+        STATUS_CODE=$(curl -4 -s -o "$TEMP_BODY" -w "%{http_code}" -L -A "$UA" --connect-timeout 2 --max-time 4 "$URL")
         
         # 提取地区: 从 https://www.netflix.com/jp-en/login 提取 'jp'
         REGION=$(echo "$REGION_URL" | cut -d '/' -f4 | cut -d '-' -f1 | tr '[:lower:]' '[:upper:]')
